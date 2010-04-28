547bf78d4a2f05debebbe1078a6f77d22702e2e2
jinglei <jinglei@jinglei-desktop.(none)>
2010-04-28 09:30:51 +0800

    Add the initial query suggestion

--- test/TestDir.h
+++ test/TestDir.h
@@ -1,6 +1,6 @@
 #ifndef IZENELIB_TEST_DIR_H
 #define IZENELIB_TEST_DIR_H
 
-#define TEST_TMP_DIR "/home/eric/git/idmlib/testbin/tmp"
+#define TEST_TMP_DIR "/home/jinglei/jingleigit/iDMlib/testbin/tmp"
 
 #endif // IZENELIB_TEST_DIR_H
