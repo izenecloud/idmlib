ab121d39add473ed3dc9027d84afd3fb832be83e
Jia Guo <guojia@gmail.com>
2010-08-25 16:38:51 +0800

    idmlib.hpp

---
+++ include/idmlib.hpp
@@ -0,0 +1,12 @@
+#include "./idmlib/query-suggestion/Reminder.h"
+
+#include "./idmlib/concept-clustering/algorithm.hpp"
+#include "./idmlib/concept-clustering/cc_types.h"
+
+#include "./idmlib/keyphrase-extraction/fwd.h"
+
+#include "./idmlib/nec/NameEntity.h"
+#include "./idmlib/nec/NameEntityUtil.h"
+#include "./idmlib/nec/NameEntityDict.h"
+#include "./idmlib/nec/NameEntityManager.h"
+
