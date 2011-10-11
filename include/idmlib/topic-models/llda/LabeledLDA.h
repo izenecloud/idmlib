67d016ec5ead2e763fc788baf529c2650f5f2cc1
Yingfeng Zhang <yingfeng.zhang@gmail.com>
2011-10-11 17:56:17 +0800

    add LabeledLDA

---
+++ include/idmlib/topic-models/llda/LabeledLDA.h
@@ -0,0 +1,68 @@
+#ifndef LABELED_LDA_H__
+#define LABELED_LDA_H__
+
+#include <idmlib/topic-models/common/samplib.h>
+#include <idmlib/topic-models/common/stats.h>
+
+#include <string>
+
+class LabeledLDA
+{
+public:
+    std::string training_data;
+    std::string label_data;
+
+    int D;
+    int K;
+    int** M;
+    int* N;
+    int V;
+    double alpha;
+    double eta;
+    double** post_beta;
+    double** post_theta;
+    int*** w;
+    int*** z;
+    int*** best_z;
+    int** labels;
+    int nIter;
+    int dumpInterval;
+
+    LabeledLDA(const std::string& trainingfile, const std::string& labelfile, int iter);
+
+    ~LabeledLDA();
+
+    void estimate();
+
+private:
+    void load_model();
+
+    void load_labels();
+
+    void initialize_model();
+
+    void initialize_alpha();
+
+    void initialize_eta();
+
+    void initialize_z();
+
+    void initialize_post_beta();
+
+    void initialize_post_theta();
+
+    void resample_post_beta();
+
+    void resample_post_theta();
+
+    void resample_z();
+
+    double compute_log_posterior();
+
+    void dump_z();
+
+    void clean_model();
+};
+
+#endif
+
---
+++ test/topic-models/llda.cc
@@ -0,0 +1,30 @@
+#include <idmlib/topic-models/llda/LabeledLDA.h>
+
+/****************************** MAIN ******************************/
+
+int main(int ARGC, char *ARGV[])
+{
+    int nIter;
+    int dumpInterval;
+
+    if (ARGC != 5)
+    {
+        fprintf(stderr, "usage %s <entities> <labels> <nIter> <dumpInterval>\n", ARGV[0]);
+        exit(1);
+    }
+
+    std::string trainingfile(ARGV[1]);
+    std::string labelfile(ARGV[2]);
+    nIter = atoi(ARGV[3]);
+    dumpInterval = atoi(ARGV[4]);
+
+    LabeledLDA lda(trainingfile,labelfile,nIter);
+
+    fprintf(stderr, "-- This program was automatically generated using HBC (v 0.7 beta) from LDA.hier\n--     see http://hal3.name/HBC for more information\n");
+    fflush(stderr);
+
+    lda.estimate();
+
+    return 0;
+}
+
