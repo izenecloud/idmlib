a7c422925f531637f0e94db5e7009901389ea3f9
Jia Guo <guojia@gmail.com>
2011-10-19 16:57:33 +0800

    little on nec test case

--- test/nec/t_NEC.cpp
+++ test/nec/t_NEC.cpp
@@ -5,12 +5,14 @@
 #include <ml/Evaluator.h>
 #include <idmlib/nec/NameEntityDict.h>
 #include <idmlib/nec/NameEntityManager.h>
+#include <idmlib/nec/nec.h>
 #include <fstream>
 #include <string>
 #include <set>
 #include <idmlib/util/StringUtil.hpp>
 #include "../test_common.h"
 using namespace idmlib;
+using namespace idmlib::nec;
 
 
 
@@ -90,6 +92,72 @@ BOOST_AUTO_TEST_CASE(normal_test)
                 }
                 
                 break;
+            }
+            total += 1;
+        }
+    }
+    
+    double rate = correct/total;
+    BOOST_CHECK( rate >= 0.5 );
+}
+
+BOOST_AUTO_TEST_CASE(newnec_test)
+{
+    std::string res_path;
+    BOOST_CHECK( ResourceHelper::getNecPath(res_path) == true );
+    idmlib::nec::NEC nec;
+    nec.Load(res_path);
+    typedef std::pair<uint32_t, uint32_t> id2count_t;
+    typedef std::pair<izenelib::util::UString, uint32_t> str2count_t;
+    std::vector<id2count_t> b(1, std::make_pair(1,1));
+    std::vector<str2count_t> c;
+    std::vector<str2count_t> d;
+    double correct = 0.0;
+    uint32_t total = 0;
+    {
+        
+        std::vector<izenelib::util::UString> inputList;
+        inputList.push_back( izenelib::util::UString("张国荣", izenelib::util::UString::UTF_8) );
+        
+        for(uint32_t w=0;w<inputList.size();w++)
+        {
+            NECItem item( inputList[w], b,c,d );
+            int result = nec.Predict(item);
+            if( result==1 )
+            {
+                correct += 1;
+            }
+            total += 1;
+        }
+    }
+    
+    {
+        std::vector<izenelib::util::UString> inputList;
+        inputList.push_back( izenelib::util::UString("中国", izenelib::util::UString::UTF_8) );
+        
+        for(uint32_t w=0;w<inputList.size();w++)
+        {
+            NECItem item( inputList[w], b,c,d );
+            int result = nec.Predict(item);
+            if( result==2 )
+            {
+                correct += 1;
+            }
+            total += 1;
+        }
+    }
+    
+    {
+        std::vector<izenelib::util::UString> inputList;
+        inputList.push_back( izenelib::util::UString("人民银行", izenelib::util::UString::UTF_8) );
+        
+        for(uint32_t w=0;w<inputList.size();w++)
+        {
+            NECItem item( inputList[w], b,c,d );
+            int result = nec.Predict(item);
+            if( result==3 )
+            {
+                correct += 1;
             }
             total += 1;
         }
